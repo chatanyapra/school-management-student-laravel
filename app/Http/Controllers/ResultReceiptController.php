72c554ce1631a8e7b21cfdf6c9a00d709c42e63d
chatanyapra <chatanyapra@gmail.com>
2023-11-04 08:15:06 -0700

    reduce complexity of code

--- app/Http/Controllers/ResultReceiptController.php
+++ app/Http/Controllers/ResultReceiptController.php
@@ -5,6 +5,7 @@ use Carbon\Carbon;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\DashboardController;
 
 class ResultReceiptController extends Controller
 {
@@ -26,15 +27,14 @@ class ResultReceiptController extends Controller
         $class_result= session()->get('user_class').'result';
         $user_class_DB= session()->get('user_class').'db';
         $class_name= str_replace('_', '-', $user_class);
-        $student_detail= DB::table($user_class_DB)
-                        ->select("$user_class_DB.Sno", "$user_class_DB.Name", "$user_class_DB.photo", "$user_class_DB.fatherName", "$user_class_DB.Email", "$user_class_DB.phoneNo",
-                                    "$user_class_DB.ClassRegistration", "$user_class_DB.total_fees", "$user_class_DB.feesSubmitted", "$class_result.*")
-                            ->leftJoin("$class_result", "$class_result.student_regno", "=", "$user_class_DB.registrationNo")
-                            ->where("$user_class_DB.registrationNo","=", $user_regno)
-                            ->where("$class_result.exam_type", "=", "half-yearly")
-                            ->get()
-                        ;
-            $total_att= session()->get('net_attendance');
-        return view('big-component-files/student-result', compact('student_detail', 'class_name', 'total_att'));
+
+        $otherController = new DashboardController;
+
+        // Call the method from the other controller
+        $query = $otherController->user_info_function();
+        $result_user = $otherController->result_function();
+
+        $total_att= session()->get('net_attendance');
+        return view('big-component-files/student-result', compact('query', 'class_name', 'total_att', 'result_user'));
     }
 }
